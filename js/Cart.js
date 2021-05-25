220e575a4de4537c89fb63b5909cecd47348d808
AhmadNAwasrah <eng.ahmad.nawasreh@gmail.com>
2021-05-25 22:24:06 +0300

    cart css

--- js/Cart.js
+++ js/Cart.js
@@ -21,58 +21,58 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() { }
+
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 let tableEl;
 let delrowIn;
 let deleteItem;
-let valueEl=0;
-let billArray=[];
+let valueEl = 0;
+let billArray = [];
 
 function showCart() {
-   tableEl=document.getElementById("ta")
-  
-  // TODO: Find the table body
-  for(let i=0; i<cart.items.length; i++)
-  {  
-      let trEl=document.createElement('tr');
-      let delrowIn=i+1;
-      tableEl.appendChild(trEl)
-      let td1El=document.createElement('td')
-      trEl.appendChild(td1El)
-      td1El.textContent=cart.items[i].product;
-      trEl.setAttribute('id', '${td1El.textcontent}')
-    let td2El=document.createElement('td')
-    trEl.appendChild(td2El)
-  td2El.textContent=cart.items[i].price;
-  let td3El=document.createElement('td')
-    trEl.appendChild(td3El)
-  td3El.textContent=cart.items[i].quantity;
-  let totalpriceEl =td2El.textContent*td3El.textContent;
-  let td4El=document.createElement('td')
-    trEl.appendChild(td4El)
-  td4El.textContent=totalpriceEl;
-  let td5El=document.createElement('td')
-    trEl.appendChild(td5El)
-  let removebtEl=document.createElement('button');
-  removebtEl.setAttribute('onclick', "removeItemFromCart(this)")
-  removebtEl.textContent="x"
-  td5El.appendChild(removebtEl);
+  tableEl = document.getElementById("ta")
 
-  
-   valueEl=valueEl+totalpriceEl;
-   billArray.push(totalpriceEl);
-  
+  // TODO: Find the table body
+  for (let i = 0; i < cart.items.length; i++) {
+    let trEl = document.createElement('tr');
+    let delrowIn = i + 1;
+    tableEl.appendChild(trEl)
+    let td1El = document.createElement('td')
+    trEl.appendChild(td1El)
+    td1El.textContent = cart.items[i].product;
+    trEl.setAttribute('id', '${td1El.textcontent}')
+    let td2El = document.createElement('td')
+    trEl.appendChild(td2El)
+    td2El.textContent = cart.items[i].price;
+    let td3El = document.createElement('td')
+    trEl.appendChild(td3El)
+    td3El.textContent = cart.items[i].quantity;
+    let totalpriceEl = td2El.textContent * td3El.textContent;
+    let td4El = document.createElement('td')
+    trEl.appendChild(td4El)
+    td4El.textContent = totalpriceEl;
+    let td5El = document.createElement('td')
+    trEl.appendChild(td5El)
+    let removebtEl = document.createElement('button');
+    removebtEl.setAttribute('onclick', "removeItemFromCart(this)")
+    removebtEl.textContent = "x"
+    td5El.appendChild(removebtEl);
+
+
+    valueEl = valueEl + totalpriceEl;
+    billArray.push(totalpriceEl);
+
   }
-  
-  
-  
+
+
+
   // TODO: Iterate over the items in the cart
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
-  
+
 }
 
 
@@ -84,9 +84,9 @@ function removeItemFromCart(event) {
 
   tableEl = document.getElementById('ta');
   console.log(event.parentNode.parentNode.rowIndex)
-  cart.items.splice((event.parentNode.parentNode.rowIndex)-1,1)
-  
-  billArray.splice((event.parentNode.parentNode.rowIndex)-1,1)
+  cart.items.splice((event.parentNode.parentNode.rowIndex) - 1, 1)
+
+  billArray.splice((event.parentNode.parentNode.rowIndex) - 1, 1)
   console.log(billArray);
   tableEl.deleteRow(event.parentNode.parentNode.rowIndex);
   console.log(event.parentNode.parentNode.rowIndex);
@@ -95,34 +95,36 @@ function removeItemFromCart(event) {
   let data = JSON.stringify(cart.items);
   localStorage.setItem('cart', data);
 
-  loadCart();  
+  loadCart();
   cartupdateCount();
-         
- }
 
- let bbtnEl =document.getElementById('bbtn');
- let bbtnspanEl =document.getElementById('bill');
- 
- bbtnEl.addEventListener('click',billCalc);
- function billCalc(){
-    bbtnspanEl.textContent=valueEl;
-    let sum=0;
-    for(let i=0;i<billArray.length;i++){
-       sum=sum+billArray[i];
-    }
-    bbtnspanEl.textContent=sum;
-    // formEl.hidden = false;
-  }
+}
 
-  function cartupdateCount() {
-    cartcountEl.textContent=""
-    let cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-    let cart = new Cart(cartItems);
-    if(cart.items.length !== 0){
-      cartcountEl.textContent = `(${cart.items.length})`;
-    }
+let bbtnEl = document.getElementById('bbtn');
+let bbtnspanEl = document.getElementById('bill');
+
+bbtnEl.addEventListener('click', billCalc);
+function billCalc() {
+  bbtnspanEl.textContent = valueEl;
+  let sum = 0;
+  document.getElementById('new-user').style.display = 'block';
+  document.getElementById('track').style.display = 'block';
+  for (let i = 0; i < billArray.length; i++) {
+    sum = sum + billArray[i];
   }
-  
+  bbtnspanEl.textContent = sum;
+  // formEl.hidden = false;
+}
+
+function cartupdateCount() {
+  cartcountEl.textContent = ""
+  let cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+  let cart = new Cart(cartItems);
+  if (cart.items.length !== 0) {
+    cartcountEl.textContent = `(${cart.items.length})`;
+  }
+}
+
 cartupdateCount();
 // This will initialize the page and draw the cart on screen
 renderCart();
